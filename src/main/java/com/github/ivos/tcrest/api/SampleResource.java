54fd35d6af58a48af0daec2970be6d2c65a62e92
Ivo Maixner <ivo.maixner@gmail.com>
2015-01-19 14:45:23 +0100

    Add logging.

--- src/main/java/com/github/ivos/tcrest/api/SampleResource.java
+++ src/main/java/com/github/ivos/tcrest/api/SampleResource.java
@@ -12,12 +12,17 @@ import javax.ws.rs.core.MediaType;
 
 import ma.glasnost.orika.MapperFacade;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.github.ivos.tcrest.service.Sample;
 import com.github.ivos.tcrest.service.SampleService;
 
 @Path("projects")
 @ApplicationScoped
 public class SampleResource {
+
+	private Logger logger = LoggerFactory.getLogger(getClass());
 
 	@Inject
 	private SampleService sampleService;
@@ -28,9 +33,11 @@ public class SampleResource {
 	@GET
 	@Path("{project}/samples")
 	@Produces(MediaType.APPLICATION_JSON)
-	public List<SampleDto> getSamples(@PathParam("project") String project) {
+	public List<SampleDto> listSamples(@PathParam("project") String project) {
+		logger.info("Listing samples for project {}.", project);
 		List<Sample> samples = sampleService.getSamples(project);
 		List<SampleDto> dtos = mapper.mapAsList(samples, SampleDto.class);
+		logger.info("Listed {} samples for project {}.", dtos.size(), project);
 		return dtos;
 	}
 
